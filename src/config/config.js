846715b1a12b1cfb02e155593f3dcb958110dc98
Your Name <your-email@website.com>
2023-05-15 07:07:56 -0300

    style changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 
 module.exports={
     dataUrl: "https://www.futbolargentino.com/primera-division/tabla-de-posiciones",
-    interval: process.env.SCRAPING_INTERVAL ||3600000  ,
+    interval: process.env.SCRAPING_INTERVAL ||3600000,
     port: process.env.PORT,
     dbUser: process.env.DB_USER,
     dbPassword: process.env.DB_PASSWORD
